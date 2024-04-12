b69aa07349e07d1fc423a6eed668e7dbcce5e426
Prabhas Kumar <kujjawal629@gmail.com>
2024-04-12 19:46:55 +0530

    'final'

--- GPT_HR_Analyzer/GPT_HR_Analyzer/settings.py
+++ GPT_HR_Analyzer/GPT_HR_Analyzer/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+WHITENOISE_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
